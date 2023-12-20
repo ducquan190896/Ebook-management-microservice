61bddfbe0721144ea76aa444155c10dc4925c278
quan doan <ducquan190896@gmail.com>
2023-12-20 20:59:45 +0200

    set up microservices for ebook app

---
+++ discovery/src/main/java/com/quan/discovery/DiscoveryApplication.java
@@ -0,0 +1,14 @@
+package com.quan.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryApplication.class, args);
+	}
+
+}
