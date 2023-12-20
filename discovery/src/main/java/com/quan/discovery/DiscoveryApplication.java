fcd3a46a4b9399f144a305ba8619ce238807a106
quan doan <ducquan190896@gmail.com>
2023-12-21 01:26:00 +0200

    add discover-service

--- discovery/src/main/java/com/quan/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/quan/discovery/DiscoveryApplication.java
@@ -2,9 +2,11 @@ package com.quan.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
+
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryApplication {
 
 	public static void main(String[] args) {
