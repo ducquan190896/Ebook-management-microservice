684980e136b9be7f8a6a54d03e479c6485c153f0
quan doan <ducquan190896@gmail.com>
2023-12-22 17:00:28 +0200

    add BookReposTest to ebook service

---
+++ ebook/src/test/java/com/quan/ebook/repositories/BookReposTest.java
@@ -0,0 +1,106 @@
+package com.quan.ebook.repositories;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.quan.ebook.models.entities.Book;
+import com.quan.ebook.models.enums.FormatType;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BookReposTest {
+    @InjectMocks
+    BookRepos bookRepos;
+
+    private final String newAuthor = "new author";
+    private final String newTitle = "new title";
+    private final FormatType newFormat = FormatType.pdf;
+
+    @BeforeAll
+    public void setUp() {
+        Book book1 = Book.builder()
+                .id(UUID.randomUUID().toString())
+                .author("abc")
+                .title("abc_title")
+                .format(FormatType.epub)
+                .build();
+        Book book2 = Book.builder()
+                .id(UUID.randomUUID().toString())
+                .author("abc2")
+                .title("abc2_title")
+                .format(FormatType.epub)
+                .build();
+        bookRepos.saveBook(book1);
+        bookRepos.saveBook(book2);
+    }
+
+    @Test
+    public void BookRepos_GetBooks_ReturnBooks() {
+        List<Book> books = bookRepos.getbooks();
+        System.out.println(books.size() + " ... size");
+
+        Assertions.assertThat(books).isNotNull();
+        Assertions.assertThat(books.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void BookRepos_GetBookById_ReturnBook() {
+        Book book = getFirstBookFromRepository();
+
+        Book savedBook = bookRepos.getBookById(book.getId()).get();
+
+        Assertions.assertThat(savedBook).isNotNull();
+        Assertions.assertThat(savedBook.getId()).isEqualTo(book.getId());
+        Assertions.assertThat(savedBook.getAuthor()).isEqualTo(book.getAuthor());
+    }
+
+    @Test
+    public void BookRepos_SaveBook_ReturnUpdatedBook() {
+        Book book = getFirstBookFromRepository();
+
+        // update book
+        book.setAuthor(newAuthor);
+        book.setTitle(newTitle);
+        book.setFormat(newFormat);
+        Book updatedBook = bookRepos.saveBook(book);
+
+        Assertions.assertThat(updatedBook).isNotNull();
+        Assertions.assertThat(updatedBook.getId()).isEqualTo(book.getId());
+        Assertions.assertThat(updatedBook.getAuthor()).isEqualTo(newAuthor);
+        Assertions.assertThat(updatedBook.getTitle()).isEqualTo(newTitle);
+        Assertions.assertThat(updatedBook.getFormat()).isEqualTo(newFormat);
+    }
+
+    @Test
+    public void BookRepos_DeleteBook_ReturnBookIsEmpty() {
+        Book book = createSampleBook();
+        bookRepos.deleteBook(book);
+        Optional<Book> bookOptional = bookRepos.getBookById(book.getId());
+
+        Assertions.assertThat(bookOptional.isPresent()).isEqualTo(false);
+    }
+
+    private Book getFirstBookFromRepository() {
+        return bookRepos.getbooks().get(0);
+    }
+
+    private Book createSampleBook() {
+        Book book = Book.builder()
+                .id(UUID.randomUUID().toString())
+                .author("abc3")
+                .title("abc3_title")
+                .format(FormatType.epub)
+                .build();
+        return bookRepos.saveBook(book);
+    }
+}
